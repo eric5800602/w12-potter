61edd055db08c423a7822ad194116931c1b166e7
SymbolWu <symbolwu.cs10@nycu.edu.tw>
2022-05-12 14:07:10 +0800

    Add second green light

--- apps/potter/src/app/game.ts
+++ apps/potter/src/app/game.ts
@@ -1,7 +1,10 @@
 export class Game {
     private _total = 0;
     buy(E1: number,E2: number,E3: number,E4: number,E5: number) {
-        this._total += E1 * 100;
+        this._total += (E1+E2+E3+E4+E5) * 100;
+        if( (E1+E2+E3+E4+E5) > 1){
+            this._total = this._total*(1-(E1+E2+E3+E4+E5-1)*0.05);
+        }
     }
 
     get total() {
