039cccaf1e033413a02b00b90533a5960d063ecc
SymbolWu <symbolwu.cs10@nycu.edu.tw>
2022-05-12 14:39:37 +0800

    The second reconstruct

--- apps/potter/src/app/game.spec.ts
+++ apps/potter/src/app/game.spec.ts
@@ -1,24 +1,26 @@
 import { Game } from './game';
 
 describe('Game', () => {
+  let game: Game;
+
+  beforeEach(() => {
+    game = new Game();
+  });
   it('should create an instance', () => {
     expect(new Game()).toBeTruthy();
   });
   // game.spec.ts
   test('Buy one', () => {
-    const game = new Game();
     game.buy(1,0,0,0,0);
     expect(game.total).toBe(100);
   });
 
   test('Buy one E1 and one E2', () => {
-    const game = new Game();
     game.buy(1,1,0,0,0);
     expect(game.total).toBe(190);
   });
 
   test('Buy one E1 and two E2', () => {
-    const game = new Game();
     game.buy(1,2,0,0,0);
     expect(game.total).toBe(290);
   });
--- apps/potter/src/app/game.ts
+++ apps/potter/src/app/game.ts
@@ -2,10 +2,18 @@ export class Game {
     private _total = 0;
     buy(E1: number,E2: number,E3: number,E4: number,E5: number) {
         let books: number[] = [E1,E2,E3,E4,E5];
-        // this._total += this.SumDatareduce(books) * 100;
-        // if( this.SumDatareduce(books) > 1){
-        //     this._total = this._total*(1-(this.SumDatareduce(books)-1)*0.05);
-        // }
+        this.CalculateDiscount(books);
+    }
+
+    get total() {
+        return this._total;
+    }
+
+    public SumDatareduce(arr:number[]){
+        return arr.reduce((a,b)=>a+b);  
+    }
+
+    public CalculateDiscount(books:number[]){
         let discount: number[] = [0.95,0.9,0.8,0.75];
         while(this.SumDatareduce(books) > 0){
             var num:number = 0;
@@ -25,13 +33,5 @@ export class Game {
             }
             this._total += price_round;
         }
-    }
-
-    get total() {
-        return this._total;
-    }
-
-    public SumDatareduce(arr:number[]){
-        return arr.reduce((a,b)=>a+b);  
     }
 }
