627470898dcf6909902ca6882fbaa2dd562818a7
SymbolWu <symbolwu.cs10@nycu.edu.tw>
2022-05-12 14:16:06 +0800

    First reconstruct and add third red light

--- apps/potter/src/app/game.spec.ts
+++ apps/potter/src/app/game.spec.ts
@@ -16,4 +16,10 @@ describe('Game', () => {
     game.buy(1,1,0,0,0);
     expect(game.total).toBe(190);
   });
+
+  test('Buy one E1 and two E2', () => {
+    const game = new Game();
+    game.buy(1,2,0,0,0);
+    expect(game.total).toBe(290);
+  });
 });
--- apps/potter/src/app/game.ts
+++ apps/potter/src/app/game.ts
@@ -1,13 +1,18 @@
 export class Game {
     private _total = 0;
     buy(E1: number,E2: number,E3: number,E4: number,E5: number) {
-        this._total += (E1+E2+E3+E4+E5) * 100;
-        if( (E1+E2+E3+E4+E5) > 1){
-            this._total = this._total*(1-(E1+E2+E3+E4+E5-1)*0.05);
+        let books: number[] = [E1,E2,E3,E4,E5];
+        this._total += this.SumDatareduce(books) * 100;
+        if( this.SumDatareduce(books) > 1){
+            this._total = this._total*(1-(this.SumDatareduce(books)-1)*0.05);
         }
     }
 
     get total() {
         return this._total;
+    }
+
+    public SumDatareduce(arr:number[]){
+        return arr.reduce((a,b)=>a+b);  
     }
 }
